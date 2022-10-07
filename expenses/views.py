bbe72bb0e4f19526ac3b2f34d7aa156ba41e1146
Pyrograf <pyrograf.pl@gmail.com>
2022-10-07 08:50:37 +0200

    Added in expenses.ExpenseList sort by category or date.

--- expenses/views.py
+++ expenses/views.py
@@ -10,13 +10,17 @@ class ExpenseListView(ListView):
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        query_params = self.request.GET
+        print('$GET$', query_params)
+        print('$kwargs$', kwargs)
         queryset = object_list if object_list is not None else self.object_list
 
-        form = ExpenseSearchForm(self.request.GET)
+        form = ExpenseSearchForm(query_params)
         if form.is_valid():
             cdata = form.cleaned_data
             print('-->', cdata)
 
+            # Ordering
             name = cdata.get('name', '').strip()
             if name:
                 queryset = queryset.filter(name__icontains=name)
@@ -29,15 +33,32 @@ class ExpenseListView(ListView):
                 queryset = queryset.filter(date__lt=to_date)
 
             categories = cdata.get('categories')
-            queryset = queryset.filter(category__id__in=categories)
+            # FIXME not working with pagination and ordering
+            # queryset = queryset.filter(category__id__in=categories)
+
+        # Ordering after any forms inputs
+        order_by = query_params.get('order_by', '')
+        print(order_by)
+
+        if order_by:
+            DIR_CHARS = 4
+            field_name = order_by[:-DIR_CHARS]
+            direction = '' if order_by[-DIR_CHARS+1:].lower() == 'asc' else '-'
+            # print(field_name, direction)
+            queryset = queryset.order_by(f"{direction}{field_name}")
+
+
+        # context = {'quer': self.request.GET}
+        # print(form)
 
         return super().get_context_data(
+            # context=context,
             form=form,
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
             **kwargs)
 
+
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
-
