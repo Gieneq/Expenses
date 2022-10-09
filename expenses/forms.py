3158d08069ab8c881b1799fc1ce9c69e58ddd3f7
Pyrograf <pyrograf.pl@gmail.com>
2022-10-09 08:38:33 +0200

    Added update view for expenses.Category

--- expenses/forms.py
+++ expenses/forms.py
@@ -4,11 +4,11 @@ from .models import Expense, Category
 
 class ExpenseSearchForm(forms.ModelForm):
     _MIN_YEAR, _MAX_YEAR = 1980, 2020  # TODO better make tuple and reverse range
-    CATEGORY_CHOICES = [(category.id, category.name) for category in Category.objects.all()]
+    # CATEGORY_CHOICES = [(category.id, category.name) for category in Category.objects.all()]
 
     from_date = forms.DateField(required=False, widget=forms.SelectDateWidget(years=range(_MIN_YEAR, _MAX_YEAR + 1)))
     to_date = forms.DateField(required=False, widget=forms.SelectDateWidget(years=range(_MAX_YEAR, _MIN_YEAR - 1, -1)))
-    categories = forms.MultipleChoiceField(required=False, choices=CATEGORY_CHOICES,
+    categories = forms.MultipleChoiceField(required=False,
                                            widget=forms.CheckboxSelectMultiple)
 
     class Meta:
@@ -18,3 +18,4 @@ class ExpenseSearchForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
+        self.fields['categories'].choices = [(category.id, category.name) for category in Category.objects.all()]
